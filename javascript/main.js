81ec4180b28c68efc7f86cb87fe4e3339d6ef45e
Alexander Reva <revaalex123@gmail.com>
2023-04-03 15:06:51 +0300

    fix
    fix

---
+++ javascript/main.js
@@ -0,0 +1,26 @@
+// import '../css/main.css'
+// import '../css/reset.css'
+// import '../scss/main.scss'
+// import '../scss/nav/nav.scss'
+// import '../scss/header__hero/header__hero.scss'
+// import '../scss/section__services/section__services.scss'
+
+
+// import '../css/media.css'
+
+
+
+// import '../assets/images/hello__tag.svg'
+// import '../assets/images/my__services__tag.svg'
+// import '../assets/images/arrow.svg'
+// import '../assets/images/mobile__app__icon.svg'
+// import '../assets/images/ui-ux__icon.svg'
+// import '../assets/images/web__design__icon.svg'
+// import '../assets/images/customers__icon.svg'
+// import '../assets/images/award__icon.svg'
+// import '../assets/images/BG.jpg'
+// import '../assets/images/burger-btn.png'
+// import '../assets/images/your__images__here.png'
+
+
+// import './buttons'
